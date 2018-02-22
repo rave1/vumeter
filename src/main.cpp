622e36108bdf56d13bc7b2e7e719e059f67f7b08
Jakub <jakubsopel@protonmail.com>
2018-02-22 16:19:56 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,18 @@ float sensitivity = 1;
 int middleLed = 0;
 int prevSound = 0;
 int prevMapped = 0;
-int sound, i;
+int prevPeak = 0;
+int peak = 0;
+int sound, i, j;
 
 uint32_t red = ring.Color(255, 0, 0);
 uint32_t green = ring.Color(0, 255 ,0);
 uint32_t ringColor[16] = {red, red, red, green, green, green, green, green, green, green, green, green, green, red, red, red};
-
+uint32_t blue = ring.Color(0, 0, 255);
 void storePrev(){
     prevSound = sound;
     prevMapped = mappedValue;
+    prevPeak = peak;
 }
 void readvalues() {
     for (i = 0; i<= numofReadings; i++)
@@ -31,6 +34,7 @@ void readvalues() {
     sound /= numofReadings;
     sound *= sensitivity;
     mappedValue = map(sound, minAnalog, maxAnalog, 0, ledCount - 1);
+    peak = mappedValue + 1;
 }
 void setup() {
     // put your setup code here, to run once:
@@ -40,21 +44,24 @@ void setup() {
     ring.show();
 }
 void drawValues() {
-  for (i = 0; i < mappedValue; i++)
+  for (i = 0; i < mappedValue; i++) {
     ring.setPixelColor(i, ringColor[i]);
-    for (i = prevMapped; i >= mappedValue; i--)
-      ring.setPixelColor(i, 0);
+  //  ring.setPixelColor(peak, blue);
+  }
+  for (i = prevMapped; i >= mappedValue; i--){
+    ring.setPixelColor(i, 0);
+//    ring.setPixelColor(prevPeak, 0);
+    //ring.setPixelColor(i++, 0);
+  }
 }
 void shutoff() {
   mappedValue = 0;
   ring.show();
 }
 void loop() {
-    // put your main code here, to run repeatedly:
       readvalues();
-      Serial.println(mappedValue);
+      Serial.println(sound);
       drawValues();
       ring.show();
       storePrev();
-    //shutoff();
 }
