97c8406a9c8b94588628c61b67de1524dfef1242
Jakub <jakubsopel@protonmail.com>
2018-02-23 09:26:18 +0100

    colors

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,16 @@ int prevMapped = 0;
 int prevPeak = 0;
 int peak = 0;
 int sound, i, j;
+float brightness = 0.1;
 
-uint32_t red = ring.Color(255, 0, 0);
-uint32_t green = ring.Color(0, 255 ,0);
-uint32_t ringColor[16] = {red, red, red, green, green, green, green, green, green, green, green, green, green, red, red, red};
-uint32_t blue = ring.Color(0, 0, 255);
+uint32_t red = ring.Color(255 * brightness, 0, 0);
+uint32_t green = ring.Color(0, 255 * brightness ,0);
+uint32_t blue = ring.Color(0, 0, 255 * brightness);
+uint32_t magenta = ring.Color(230 * brightness,0 * brightness,255 * brightness);
+uint32_t turk = ring.Color(85 * brightness,31 * brightness,67 * brightness);
+uint32_t darkerturk = ring.Color(95 * brightness,30 * brightness,225 * brightness);
+uint32_t pink = ring.Color(255 * brightness,7 * brightness,120 * brightness);
+uint32_t ringColor[16] = {magenta, magenta, magenta, green, green, green, green, green, green, green, green, green, pink, pink, pink, red};
 void storePrev(){
     prevSound = sound;
     prevMapped = mappedValue;
@@ -43,17 +48,9 @@ void setup() {
     ring.begin();
     ring.show();
 }
-void drawValues() {
-  for (i = 0; i < mappedValue; i++) {
-    ring.setPixelColor(i, ringColor[i]);
-  //  ring.setPixelColor(peak, blue);
-  }
-  for (i = prevMapped; i >= mappedValue; i--){
-    ring.setPixelColor(i, 0);
-//    ring.setPixelColor(prevPeak, 0);
-    //ring.setPixelColor(i++, 0);
-  }
-}
+
+void drawValues();
+
 void shutoff() {
   mappedValue = 0;
   ring.show();
@@ -64,4 +61,16 @@ void loop() {
       drawValues();
       ring.show();
       storePrev();
+}
+
+void drawValues() {
+  for (i = 0; i < mappedValue; i++) {
+    ring.setPixelColor(i, ringColor[i]);
+  //  ring.setPixelColor(peak, blue);
+  }
+  for (i = prevMapped; i >= mappedValue; i--){
+    ring.setPixelColor(i, 0);
+//    ring.setPixelColor(prevPeak, 0);
+    //ring.setPixelColor(i++, 0);
+  }
 }
